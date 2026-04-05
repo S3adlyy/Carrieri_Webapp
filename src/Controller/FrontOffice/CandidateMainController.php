0b7e041166d9c76096eac4e303e20ea227edf744
ons-nagara <nagara.ons@esprit.tn>
2026-04-05 21:39:01 +0100

    CRUD postulation

--- src/Controller/FrontOffice/CandidateMainController.php
+++ src/Controller/FrontOffice/CandidateMainController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller\FrontOffice;
 
+use App\Entity\OffreEmploi;
 use App\Repository\OffreEmploiRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -36,6 +38,18 @@ class CandidateMainController extends AbstractController
         ]);
     }
 
+    #[Route('/offres/{id}', name: 'app_candidate_offre_show', requirements: ['id' => '\d+'])]
+    public function showOffre(OffreEmploi $offre): Response
+    {
+        if ($offre->getDateExpiration() && $offre->getDateExpiration() < new \DateTime()) {
+            throw $this->createNotFoundException('Cette offre n’est plus disponible.');
+        }
+
+        return $this->render('FrontOffice/main/offre_show.html.twig', [
+            'offre' => $offre,
+        ]);
+    }
+
     #[Route('/mission', name: 'app_candidate_mission')]
     public function mission(): Response
     {
@@ -53,4 +67,17 @@ class CandidateMainController extends AbstractController
     {
         return $this->render('FrontOffice/main/messagerie.html.twig');
     }
+
+    #[Route('/offres/{id}/postuler', name: 'app_candidate_offre_apply', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function applyOffre(OffreEmploi $offre): Response
+    {
+        if ($offre->getDateExpiration() && $offre->getDateExpiration() < new \DateTime()) {
+            throw $this->createNotFoundException('Cette offre n’est plus disponible.');
+        }
+
+        return $this->render('FrontOffice/main/offre_apply.html.twig', [
+            'offre' => $offre,
+        ]);
+    }
+    
 }
